66c92d7b905bd1fd1452cf67dc01fa139e70e6b8
creform <62896293+creform@users.noreply.github.com>
2020-04-08 22:05:33 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.9/192.168.1.2/g' package/base-files/files/bin/config_generate
+git clone https://github.com/destan19/OpenAppFilter.git package/tz
+./scripts/feeds update -a
+./scripts/feeds install -a
