db93c260c08869f43d3de202b23723b549c8d088
creform <62896293+creform@users.noreply.github.com>
2020-04-09 14:24:52 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
+# Description: DIY script for P3
+# Lisence: no
+# Author: creform
+# Blog: no
 #=================================================
-git clone https://github.com/destan19/OpenAppFilter.git package/tz
+git clone https://github.com/destan19/OpenAppFilter.git package/destan19
+git clone https://github.com/Lienol/openwrt-package package/lienol
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.9/g' package/base-files/files/bin/config_generate
